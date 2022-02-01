2912682146bae70a2483227a973c0d3a6184232d
Leonardo Nishida <leonardo.nishida@provider-it.com.br>
2022-02-01 09:48:30 -0300

    Primeiro commit

---
+++ HelloKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+
+var outsideName: String = "Andre";
+
+fun main(args: Array<String>) {
+    //println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    //println("Program arguments: ${args.joinToString()}")
+
+    //Variables
+    val name: String = "Leonardo"; //val nao pode ser alterado
+    var name2: String = "Amanda"; //var pode ser alterado
+
+    println(name2)
+    name2 = "Marcelo"
+    println(name2)
+
+    println(outsideName)
+}
